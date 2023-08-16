399cfeaf1e06ad628f587b780d473f587a0385d6
codehole <87037896+codalannce@users.noreply.github.com>
2023-08-16 17:48:58 +0300

    Update yer_istasyonu.ino

--- yer_istasyonu.ino
+++ yer_istasyonu.ino
@@ -3,12 +3,12 @@
 
 SoftwareSerial mySerial(11, 10); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
 LoRa_E22 E22(&mySerial);
-
+ 
 #define M0 7
 #define M1 6
 
 struct veriler {
-
+  byte  testid[4];
   byte   paalt[4];
 
   byte   pitchdeger[4];
@@ -21,6 +21,7 @@ struct veriler {
   byte   glat[6];
   byte   glng[6];
   byte   galt[6];
+  
 
 } data;
 
@@ -42,24 +43,32 @@ void loop() {
     // Gelen mesaj okunuyor
     ResponseStructContainer rsc = E22.receiveMessage(sizeof(veriler));
     struct veriler data = *(veriler*) rsc.data;
+    float testidvalue = *(float*)(data.testid);
 
-    Serial.print(*(int*)(data.paalt));//Basınç irtifa değeri
-    Serial.print(";"); 
-    Serial.print(*(float*)(data.pitchdeger)); // Pitch değeri
-    Serial.print(";");
-    Serial.print(*(float*)(data.rolldeger)); // Roll değeri
-    Serial.print(";");
-    Serial.print(*(float*)(data.lat)); // GPS Enlem değeri
-    Serial.print(";");
-    Serial.print(*(float*)(data.lng)); // GPS Boylam Değeri
-    Serial.print(";");
-    Serial.print(*(float*)(data.alt)); // GPS irtifa değeri
-    Serial.print(",");
-    Serial.print(*(float*)(data.glat)); // Görev yükü enlem değeri
-    Serial.print(";");
-    Serial.print(*(float*)(data.glng)); // Görev yükü boylam değeri
-    Serial.print(";");
-    Serial.println(*(float*)(data.galt)); // Görev yükü irtifa değeri
+    if(testidvalue == 10) {
+      Serial.print(*(int*)(data.paalt));//Basınç irtifa değeri
+      Serial.print(";"); 
+      Serial.print(*(float*)(data.pitchdeger)); // Pitch değeri
+      Serial.print(";");
+      Serial.print(*(float*)(data.rolldeger)); // Roll değeri
+      Serial.print(";");
+      Serial.print(*(float*)(data.lat)); // GPS Enlem değeri
+      Serial.print(";");
+      Serial.print(*(float*)(data.lng)); // GPS Boylam Değeri
+      Serial.print(";");
+      Serial.println(*(float*)(data.alt)); // GPS irtifa değeri
+    }
+    
+    
+    if (testidvalue == 5) {
+      Serial.print(*(float*)(data.glat)); // Görev yükü enlem değeri
+      Serial.print(";");
+      Serial.print(*(float*)(data.glng)); // Görev yükü boylam değeri
+      Serial.print(";");
+      Serial.println(*(float*)(data.galt)); // Görev yükü irtifa değeri
+
+    }
+    delay(300);
     rsc.close();
 
   }
